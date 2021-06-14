b10da999568a482031605a9a1665fea1fdf7b001
Brinthan <brinthsega@gmail.com>
2021-06-14 23:29:16 +0530

    changes 14/06

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from "rxjs";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  title: string = 'Todo App';
+  showAddTask: boolean = false;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  ngOnInit(): void { }
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+}
